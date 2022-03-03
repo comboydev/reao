2139b898d3056ec9ac85b59b76380ce3e1c4e2fc
dragon1211 <inddev0807@gmail.com>
2022-03-03 00:17:23 -0800

    fixed product add page

--- frontend/src/views-admin/app-views/apps/product/ProductForm/index.js
+++ frontend/src/views-admin/app-views/apps/product/ProductForm/index.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react'
+import { useHistory, useLocation } from "react-router-dom"
 import PageHeaderAlt from 'components/layout-components/PageHeaderAlt'
 import { Tabs, Form, Button, message } from 'antd';
 import Flex from 'components/shared-components/Flex'
 import GeneralField from './GeneralField'
-import VariationField from './VariationField'
-import ShippingField from './ShippingField'
 import ProductListData from "assets/data/product-list.data.json"
 
 const { TabPane } = Tabs;
@@ -21,11 +20,13 @@ const EDIT = 'EDIT'
 const ProductForm = props => {
 
 	const { mode = ADD, param } = props
+	const history = useHistory();
+	const location = useLocation();
 
 	const [form] = Form.useForm();
-	const [uploadedImg, setImage] = useState('')
-	const [uploadLoading, setUploadLoading] = useState(false)
-	const [submitLoading, setSubmitLoading] = useState(false)
+	const [coin_images, setCoinImages] = useState([{uri: ''}]);
+	const [submit_loading, setSubmitLoading] = useState(false);
+
 
 	useEffect(() => {
     	if(mode === EDIT) {
@@ -36,34 +37,55 @@ const ProductForm = props => {
 			const productData = ProductListData.filter( product => product.id === produtId)
 			const product = productData[0]
 			form.setFieldsValue({
-				comparePrice: 0.00,
-				cost: 0.00,
-				taxRate: 6,
-				description: 'There are many variations of passages of Lorem Ipsum available.',
-				category: product.category,
-				name: product.name,
-				price: product.price
+				// comparePrice: 0.00,
+				// cost: 0.00,
+				// taxRate: 6,
+				// description: 'There are many variations of passages of Lorem Ipsum available.',
+				// category: product.category,
+				// name: product.name,
+				// price: product.price
 			});
-			setImage(product.image)
+			setCoinImages(product.image)
+		} else if(mode === ADD){
+			form.setFieldsValue({
+				minNumberOfCoins: 10,
+				taxRate: 10,
+				price: 10000
+			})
 		}
   	}, [form, mode, param, props]);
 
-	const handleUploadChange = info => {
-		if (info.file.status === 'uploading') {
-			setUploadLoading(true)
-			return;
-		}
-		if (info.file.status === 'done') {
-			getBase64(info.file.originFileObj, imageUrl =>{
-				setImage(imageUrl)
-				setUploadLoading(true)
-			});
-		}
+
+	const addCoinImage = () => {
+		let arr = coin_images;
+		arr.push({ uri: ''});
+		history.push(location.pathname);
+	}
+
+	const removeCoinImage = (index) => {
+		let arr = coin_images;
+		arr.splice(index);
+		setCoinImages(arr);
+		history.push(location.pathname);
+	}
+
+	const handleUploadChange = (info, idx) => {
+		getBase64(info.file.originFileObj, imageUrl =>{
+			let arr = coin_images;
+			arr[idx].uri = imageUrl;
+			setCoinImages(arr);
+		});
+		history.push(location.pathname);
 	};
 
-	const onFinish = () => {
+	const onClear = () => {
+		window.location.reload();
+	}
+
+	const onSubmit = () => {
 		setSubmitLoading(true)
 		form.validateFields().then(values => {
+			console.log(values);
 			setTimeout(() => {
 				setSubmitLoading(false)
 				if(mode === ADD) {
@@ -98,8 +120,8 @@ const ProductForm = props => {
 						<Flex className="py-2" mobileFlex={false} justifyContent="between" alignItems="center">
 							<h2 className="mb-3">{mode === 'ADD'? 'Add New Product' : `Edit Product`} </h2>
 							<div className="mb-3">
-								<Button className="mr-2">Discard</Button>
-								<Button type="primary" onClick={() => onFinish()} htmlType="submit" loading={submitLoading} >
+								<Button className="mr-2" onClick={onClear}>Clear</Button>
+								<Button type="primary" onClick={onSubmit} htmlType="submit" loading={submit_loading} >
 									{mode === 'ADD'? 'Add' : `Save`}
 								</Button>
 							</div>
@@ -110,17 +132,18 @@ const ProductForm = props => {
 					<Tabs defaultActiveKey="1" style={{marginTop: 30}}>
 						<TabPane tab="General" key="1">
 							<GeneralField 
-								uploadedImg={uploadedImg} 
-								uploadLoading={uploadLoading} 
+								coin_images={coin_images}
+								addCoinImage = {addCoinImage}
+								removeCoinImage = {removeCoinImage}
 								handleUploadChange={handleUploadChange}
 							/>
 						</TabPane>
-						<TabPane tab="Variation" key="2">
+						{/* <TabPane tab="Variation" key="2">
 							<VariationField />
-						</TabPane>
-						<TabPane tab="Shipping" key="3">
+						</TabPane> */}
+						{/* <TabPane tab="Shipping" key="3">
 							<ShippingField />
-						</TabPane>
+						</TabPane> */}
 					</Tabs>
 				</div>
 			</Form>
