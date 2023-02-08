9470f0e61c2a0a49d7b386ede67591e3da37db98
dragon1211 <inddev0807@gmail.com>
2023-02-08 22:31:35 +0900

    fix header

--- client/src/system/admin/app/setting/index.js
+++ client/src/system/admin/app/setting/index.js
@@ -1,5 +1,9 @@
 import React, { lazy, Component } from 'react'
-import { UserOutlined, LockOutlined, CreditCardOutlined } from '@ant-design/icons';
+import {
+	UserOutlined,
+	LockOutlined,
+	// CreditCardOutlined,
+} from '@ant-design/icons';
 import { Menu } from 'antd';
 import { Link, Redirect, Route, Switch } from 'react-router-dom';
 import InnerAppLayout from 'system/admin/layouts/inner-app-layout';
@@ -21,11 +25,11 @@ const SettingOption = ({ match, location }) => {
 				<span>Password</span>
 				<Link to={'password'} />
 			</Menu.Item>
-			<Menu.Item key={`${match.url}/bank`}>
+			{/* <Menu.Item key={`${match.url}/bank`}>
 				<CreditCardOutlined />
 				<span>Bank</span>
 				<Link to={`bank`} />
-			</Menu.Item>
+			</Menu.Item> */}
 		</Menu>
 	);
 };
@@ -36,7 +40,7 @@ const SettingContent = ({ match }) => {
 			<Redirect exact from={`${match.url}`} to={`${match.url}/profile`} />
 			<Route path={`${match.url}/profile`} component={lazy(() => import('./profile'))} />
 			<Route path={`${match.url}/password`} component={lazy(() => import('./password'))} />
-			<Route path={`${match.url}/bank`} component={lazy(() => import('./bank'))} />
+			{/* <Route path={`${match.url}/bank`} component={lazy(() => import('./bank'))} /> */}
 		</Switch>
 	)
 }
@@ -44,12 +48,12 @@ const SettingContent = ({ match }) => {
 export class Setting extends Component {
 	render() {
 		return (
-			<InnerAppLayout 
+			<InnerAppLayout
 				sideContentWidth={320}
-				sideContent={<SettingOption {...this.props}/>}
-				mainContent={<SettingContent {...this.props}/>}
+				sideContent={<SettingOption {...this.props} />}
+				mainContent={<SettingContent {...this.props} />}
 			/>
-    );
+		);
 	}
 }
 
